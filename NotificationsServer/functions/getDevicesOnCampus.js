94d3690861861ce326a41dd0dd218e197288177a
danieltoo <torresestradadaniel@gmail.com>
2017-11-18 18:20:22 -0600

    Agregué Docker compose

--- NotificationsServer/functions/getDevicesOnCampus.js
+++ NotificationsServer/functions/getDevicesOnCampus.js
@@ -1,25 +1,32 @@
 
 var cb = require('ocb-sender')
 var ngsi = require('ngsi-parser')
+
 cb.config('http://207.249.127.149',1026,'v2')
+.then((result) => console.log(result))
+.catch((err) => console.log(err))
 
 module.exports = async function getDevicesOnCampus(location) {
 	let devicesList = []
+	if (location !== undefined){
+		let query = ngsi.createQuery({
+		  id: ".*",
+		  type: "Device",
+		  georel : "coveredBy",
+		  geometry:"polygon",
+		  coords : location,
+		  options: "keyValues"
+		})
 
-	let query = ngsi.createQuery({
-	  id: ".*",
-	  type: "Device",
-	  georel : "coveredBy",
-	  geometry:"polygon",
-	  coords : location,
-	  options: "keyValues"
-	})
-
-	await cb.getWithQuery(query)
-	.then(async (result) => {
-	    await result.map((device) =>{ 
-	    	devicesList.push(device.id)
-	    })
-	})
+		await cb.getWithQuery(query)
+		.then(async (result) => {
+			console.log(result)
+		    await result.map((device) =>{ 
+		    	devicesList.push(device.id)
+		    })
+		})
+		.catch((err) => console.log(err))
+	}
+	
 	return devicesList
 }
