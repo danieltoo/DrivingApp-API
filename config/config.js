fa7953dc66b59a175d794b637506588eb3b28d20
danieltoo <torresestradadaniel@gmail.com>
2017-11-25 18:40:06 -0600

    Cambios para heroku

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	'context' : '207.249.127.149',
-	//'context' : '207.249.127.149',
     'secret': 'Sdk.fiware2017',  //secret key for our JSON Web Token.
     'database': 'mongodb://admin:admin@ds125195.mlab.com:25195/smartsecurityfdm'
     //'database':'mongodb://localhost:27017/smartsecurityfdm'
